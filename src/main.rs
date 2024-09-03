7e0fe12d9f9b262b997531c1826398fcba8cde25
Antoine Clarman <antoinereims28@gmail.com>
2024-09-03 19:05:52 +0200

    feat: implement real checker

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::{
     env,
     io::{self, Read},
-    process::exit,
 };
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
 fn parse_str_to_i32(str: &str) -> Vec<i32> {
     let mut parsed_str = vec![];
@@ -23,25 +29,130 @@ fn get_skyscraper_grid() -> Vec<i32> {
     parse_str_to_i32(&buf)
 }
 
-fn check_rules(skyscraper_rules: &[i32]) {
+fn check_rules(skyscraper_rules: &[i32]) -> Result<(), String> {
     if skyscraper_rules.len() % 4 != 0 {
-        println!("invalid rules");
-        exit(1);
+        Err("invalid rules".to_string())
+    } else {
+        Ok(())
     }
 }
 
-fn check_grid(skyscraper_rules: &[i32], skyscraper_grid: &[i32]) {
-    if (skyscraper_rules.len() / 4) * (skyscraper_rules.len() / 4) != skyscraper_grid.len() {
-        println!("invalid grid");
-        exit(1);
+fn get_skyscraper_line(
+    direction: &Direction,
+    index: usize,
+    grid_len: usize,
+    skyscraper_grid: &[i32],
+) -> Vec<i32> {
+    let mut skyscraper_line = vec![];
+    match direction {
+        Direction::Up => {
+            for (i, skyscraper) in skyscraper_grid.iter().enumerate() {
+                if i % grid_len == index {
+                    skyscraper_line.push(*skyscraper);
+                }
+            }
+        }
+        Direction::Down => {
+            for (i, skyscraper) in skyscraper_grid.iter().enumerate().rev() {
+                if i % grid_len == index {
+                    skyscraper_line.push(*skyscraper);
+                }
+            }
+        }
+        Direction::Left => {
+            for (i, skyscraper) in skyscraper_grid.iter().enumerate() {
+                if i / grid_len == index {
+                    skyscraper_line.push(*skyscraper);
+                }
+            }
+        }
+        Direction::Right => {
+            for (i, skyscraper) in skyscraper_grid.iter().enumerate().rev() {
+                if i / grid_len == index {
+                    skyscraper_line.push(*skyscraper);
+                }
+            }
+        }
+    }
+    skyscraper_line
+}
+
+fn check_line(rule: i32, skyscraper_line: &[i32]) -> Result<(), String> {
+    let mut count_skyscraper = 0;
+    let mut max_skyscraper_size = 0;
+
+    for skyscraper_size in skyscraper_line {
+        if skyscraper_size > &max_skyscraper_size {
+            max_skyscraper_size = *skyscraper_size;
+            count_skyscraper += 1;
+        }
+    }
+    if count_skyscraper == rule {
+        Ok(())
+    } else {
+        Err(format!(
+            "Invalid Line: rule={rule}, count={count_skyscraper}, line={skyscraper_line:?}"
+        ))
     }
 }
 
-fn main() {
+fn choice_skyscraper_line(
+    i: usize,
+    grid_len: usize,
+    skyscraper_grid: &[i32],
+) -> Result<Vec<i32>, &str> {
+    if i < grid_len {
+        Ok(get_skyscraper_line(
+            &Direction::Up,
+            i % 4,
+            grid_len,
+            skyscraper_grid,
+        ))
+    } else if i < 2 * grid_len {
+        Ok(get_skyscraper_line(
+            &Direction::Down,
+            (i - grid_len) % 4,
+            grid_len,
+            skyscraper_grid,
+        ))
+    } else if i < 3 * grid_len {
+        Ok(get_skyscraper_line(
+            &Direction::Left,
+            (i - 2 * grid_len) % 4,
+            grid_len,
+            skyscraper_grid,
+        ))
+    } else if i < 4 * grid_len {
+        Ok(get_skyscraper_line(
+            &Direction::Right,
+            (i - 3 * grid_len) % 4,
+            grid_len,
+            skyscraper_grid,
+        ))
+    } else {
+        Err("Internal Error: invalid index")
+    }
+}
+
+fn check_grid(skyscraper_rules: &[i32], skyscraper_grid: &[i32]) -> Result<(), String> {
+    let grid_len = skyscraper_rules.len() / 4;
+
+    if grid_len * grid_len != skyscraper_grid.len() {
+        return Err("invalid grid".to_string());
+    }
+    for (i, rule) in skyscraper_rules.iter().enumerate() {
+        let line = choice_skyscraper_line(i, grid_len, skyscraper_grid)?;
+        check_line(*rule, &line)?;
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
     let skyscraper_rules = get_skyscraper_rules();
-    check_rules(&skyscraper_rules);
+    check_rules(&skyscraper_rules)?;
     let skyscraper_grid = get_skyscraper_grid();
-    check_grid(&skyscraper_rules, &skyscraper_grid);
+    check_grid(&skyscraper_rules, &skyscraper_grid)?;
+    Ok(())
 }
 
 #[cfg(test)]
@@ -52,5 +163,127 @@ mod tests {
     fn test_parse_str_to_i32() {
         assert_eq!(parse_str_to_i32(&String::from("1 2 3")), vec![1, 2, 3]);
         assert_eq!(parse_str_to_i32(&String::from("")), vec![]);
+    }
+
+    #[test]
+    fn test_check_rules() {
+        assert!(check_rules(&vec![1, 1, 1, 1]).is_ok());
+        assert!(check_rules(&vec![2, 1, 1, 2, 2, 1, 1, 2]).is_ok());
+
+        assert!(check_rules(&vec![1]).is_err());
+        assert!(check_rules(&vec![1, 2, 3, 4, 5]).is_err());
+    }
+
+    #[test]
+    fn test_get_skyscraper_line_one_square() {
+        assert_eq!(get_skyscraper_line(&Direction::Up, 0, 1, &vec![1]), vec![1]);
+        assert_eq!(
+            get_skyscraper_line(&Direction::Down, 0, 1, &vec![1]),
+            vec![1]
+        );
+        assert_eq!(
+            get_skyscraper_line(&Direction::Left, 0, 1, &vec![1]),
+            vec![1]
+        );
+        assert_eq!(
+            get_skyscraper_line(&Direction::Right, 0, 1, &vec![1]),
+            vec![1]
+        );
+    }
+
+    #[test]
+    fn test_get_skyscraper_line_two_square() {
+        let skyscraper_grid = vec![1, 2, 3, 4];
+
+        assert_eq!(
+            get_skyscraper_line(&Direction::Up, 0, 2, &skyscraper_grid),
+            vec![1, 3]
+        );
+        assert_eq!(
+            get_skyscraper_line(&Direction::Down, 0, 2, &skyscraper_grid),
+            vec![3, 1]
+        );
+        assert_eq!(
+            get_skyscraper_line(&Direction::Left, 0, 2, &skyscraper_grid),
+            vec![1, 2]
+        );
+        assert_eq!(
+            get_skyscraper_line(&Direction::Right, 0, 2, &skyscraper_grid),
+            vec![2, 1]
+        );
+
+        assert_eq!(
+            get_skyscraper_line(&Direction::Up, 1, 2, &skyscraper_grid),
+            vec![2, 4]
+        );
+        assert_eq!(
+            get_skyscraper_line(&Direction::Down, 1, 2, &skyscraper_grid),
+            vec![4, 2]
+        );
+        assert_eq!(
+            get_skyscraper_line(&Direction::Left, 1, 2, &skyscraper_grid),
+            vec![3, 4]
+        );
+        assert_eq!(
+            get_skyscraper_line(&Direction::Right, 1, 2, &skyscraper_grid),
+            vec![4, 3]
+        );
+    }
+
+    #[test]
+    fn test_check_line() {
+        assert!(check_line(1, &vec![1]).is_ok());
+        assert!(check_line(2, &vec![1]).is_err());
+
+        assert!(check_line(1, &vec![4, 3, 2, 1]).is_ok());
+        assert!(check_line(2, &vec![3, 4, 2, 1]).is_ok());
+        assert!(check_line(3, &vec![2, 3, 4, 1]).is_ok());
+        assert!(check_line(4, &vec![1, 2, 3, 4]).is_ok());
+
+        assert!(check_line(4, &vec![4, 3, 2, 1]).is_err());
+        assert!(check_line(3, &vec![3, 4, 2, 1]).is_err());
+        assert!(check_line(2, &vec![2, 3, 4, 1]).is_err());
+        assert!(check_line(1, &vec![1, 2, 3, 4]).is_err());
+    }
+
+    #[test]
+    fn test_choice_skyscraper_line() {
+        let grid = vec![1];
+
+        assert_eq!(choice_skyscraper_line(0, 1, &grid), Ok(vec![1]));
+        assert_eq!(choice_skyscraper_line(1, 1, &grid), Ok(vec![1]));
+        assert_eq!(choice_skyscraper_line(2, 1, &grid), Ok(vec![1]));
+        assert_eq!(choice_skyscraper_line(3, 1, &grid), Ok(vec![1]));
+        assert!(choice_skyscraper_line(4, 1, &grid).is_err());
+
+        let grid = vec![1, 2, 2, 1];
+
+        assert_eq!(choice_skyscraper_line(0, 2, &grid), Ok(vec![1, 2]));
+        assert_eq!(choice_skyscraper_line(1, 2, &grid), Ok(vec![2, 1]));
+        assert_eq!(choice_skyscraper_line(2, 2, &grid), Ok(vec![2, 1]));
+        assert_eq!(choice_skyscraper_line(3, 2, &grid), Ok(vec![1, 2]));
+        assert_eq!(choice_skyscraper_line(4, 2, &grid), Ok(vec![1, 2]));
+        assert_eq!(choice_skyscraper_line(5, 2, &grid), Ok(vec![2, 1]));
+        assert_eq!(choice_skyscraper_line(6, 2, &grid), Ok(vec![2, 1]));
+        assert_eq!(choice_skyscraper_line(7, 2, &grid), Ok(vec![1, 2]));
+        assert!(choice_skyscraper_line(8, 2, &grid).is_err());
+    }
+
+    #[test]
+    fn test_check_grid() {
+        let rules = vec![1, 1, 1, 1];
+        let grid = vec![1];
+
+        assert!(check_grid(&rules, &grid).is_ok());
+
+        let rules = vec![2, 1, 1, 2, 2, 1, 1, 2];
+        let grid = vec![1, 2, 2, 1];
+
+        assert!(check_grid(&rules, &grid).is_ok());
+
+        let rules = vec![1, 2, 3, 4];
+        let grid = vec![1];
+
+        assert!(check_grid(&rules, &grid).is_err());
     }
 }
